203d034359c6f6824050dcc432aeae0141b3c6a2
Jeremias Castañeda Arreola <jeremias.janai@outlook.com>
2018-10-18 21:23:07 -0500

    empezamos rafa

---
+++ src/js/app.js
@@ -0,0 +1,12 @@
+var resultado = document.getElementById('resultado')
+
+var suma = document.getElementById ('suma')
+var resta = document.getElementById ('resta')
+var multiplica = document.getElementById ('multiplica')
+var divide  = document.getElementById ('divide')
+
+
+
+function suma () {
+    
+}
